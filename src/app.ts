ae10b85b32cfe0b9d53d1e7fc43ffee8972326df
Joerg Woehrle <joerg.woehrle@metafinanz.de>
2017-09-06 09:53:11 +0200

    Move to typescript

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+import * as restify from 'restify';
+import * as builder from 'botbuilder';
 
-// Setup Restify Server
-var server = restify.createServer();
+
+
+const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-
-// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -18,7 +17,7 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    let message = session.message.text;
+    let message = session.message.text ? session.message.text : '';
     if ( message.toLocaleLowerCase().indexOf('uhr') > -1) {
         session.send("Es ist jetzt: %s", new Date());
     } else {
