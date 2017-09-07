ae504b3134dd601a558737a0dc7ad76eb83332d6
Joerg Woehrle <joerg.woehrle@metafinanz.de>
2017-09-07 10:22:47 +0200

    tweaks for first push

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
@@ -16,11 +16,11 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
+const bot = new builder.UniversalBot(connector, function (session) {
     let message = session.message.text ? session.message.text : '';
     if ( message.toLocaleLowerCase().indexOf('time') > -1) {
         let now = new Date();
-        session.send("The time is now exactly: %s", now.getHours() + ":" + now.getMinutes());
+        session.send("The current time is %s", now.getHours() + ":" + now.getMinutes());
     } else {
         session.send("You said: %s", session.message.text);
     }
